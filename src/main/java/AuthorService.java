33bc75fd2d62cecfce89979da42f6c682a96339f
asharifi90 <asharifibb@gmail.com>
2024-01-11 05:40:05 +0330

    add authorRepository object, call save methode on this object

--- src/main/java/AuthorService.java
+++ src/main/java/AuthorService.java
@@ -1,6 +1,10 @@
+import java.sql.SQLException;
+
 public class AuthorService {
 
-    public void register(String firstname, String lastname, int age){
+    public void register(String firstname, String lastname, int age) throws SQLException {
         Author author = new Author(firstname, lastname, age);
+        AuthorRepository authorRepository = new AuthorRepository();
+        authorRepository.save(author);
     }
 }
