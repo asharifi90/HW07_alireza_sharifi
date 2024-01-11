f405003821677717547e2158120b75596a292ce6
asharifi90 <asharifibb@gmail.com>
2024-01-11 05:04:03 +0330

    add exception

--- src/main/java/JDBCConnection.java
+++ src/main/java/JDBCConnection.java
@@ -1,8 +1,11 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class JDBCConnection {
 
     private final Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "BlackHead23");
 
+    public JDBCConnection() throws SQLException {
+    }
 }
