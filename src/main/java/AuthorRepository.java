9806084b4b51c5a867d1fe4e1bf4128a48a0e26e
asharifi90 <asharifibb@gmail.com>
2024-01-11 05:11:07 +0330

    add exception to JDBCConnection object

--- src/main/java/AuthorRepository.java
+++ src/main/java/AuthorRepository.java
@@ -1,6 +1,12 @@
+import java.sql.SQLException;
+
 public class AuthorRepository {
 
     private final JDBCConnection jdbcConnection = new JDBCConnection();
+
+    public AuthorRepository() throws SQLException {
+    }
+
     public void save(Author author){
 
     }
