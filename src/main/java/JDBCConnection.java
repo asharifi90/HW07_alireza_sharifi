2d71112b6959ae13d753f7b6e1058bb91a7afe82
asharifi90 <asharifibb@gmail.com>
2024-01-11 05:03:40 +0330

    add connection object from Connection class

--- src/main/java/JDBCConnection.java
+++ src/main/java/JDBCConnection.java
@@ -1,2 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class JDBCConnection {
+
+    private final Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "BlackHead23");
+
 }
