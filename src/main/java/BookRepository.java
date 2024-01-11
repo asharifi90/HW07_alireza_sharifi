225837b8daaefcf107d9e9002ee2369af61353fa
asharifi90 <asharifibb@gmail.com>
2024-01-11 07:07:23 +0330

    Create load methode

--- src/main/java/BookRepository.java
+++ src/main/java/BookRepository.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BookRepository {
@@ -22,9 +23,26 @@ public class BookRepository {
         preparedStatement.executeUpdate();
 
     }
-//    public Book load (int bookId){
-//
-//    }
+
+    public Book load(int bookId) throws SQLException {
+        Connection connection = jdbcConnection.getConnection();
+
+        String loadAuthor = "SELECT * FROM author WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(loadAuthor);
+        preparedStatement.setInt(1, bookId);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if(resultSet.next()) {
+            String title = resultSet.getString("title");
+            int year = resultSet.getInt("year");
+            String author = resultSet.getString("writer");
+            Book book = new Book(title, year, author);
+            return book;
+        } else {
+            return null;
+        }
+    }
+
     public void delete(Book book){
 
     }
