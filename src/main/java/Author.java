fd532c77fca38c31774e2c2bc59fd8683e0cc3c1
asharifi90 <asharifibb@gmail.com>
2024-01-11 04:56:24 +0330

    Create Author constructer with 3 fields

--- src/main/java/Author.java
+++ src/main/java/Author.java
@@ -26,6 +26,12 @@ public class Author {
         this.bookList = bookList;
     }
 
+    public Author(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
     public int getId() {
         return id;
     }
