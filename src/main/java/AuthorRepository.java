a8f441a3064673d44c79c9ee7bd28642c10ebaef
asharifi90 <asharifibb@gmail.com>
2024-01-11 05:23:26 +0330

    Complete save methode and save firstname, lastname and age of author to the table

--- src/main/java/AuthorRepository.java
+++ src/main/java/AuthorRepository.java
@@ -1,3 +1,5 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class AuthorRepository {
@@ -7,8 +9,18 @@ public class AuthorRepository {
     public AuthorRepository() throws SQLException {
     }
 
-    public void save(Author author){
+    public void save(Author author) throws SQLException {
+        Connection connection = jdbcConnection.getConnection();
 
+        String addAuthor = "INSERT INTO author(first_name, last_name, age) VALUES(?, ?, ?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(addAuthor);
+
+
+        preparedStatement.setString(1, author.getFirstName());
+        preparedStatement.setString(2, author.getLastName());
+        preparedStatement.setInt(3, author.getAge());
+
+        preparedStatement.executeUpdate();
     }
 
     public Author load(int authorId){
