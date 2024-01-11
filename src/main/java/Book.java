f25d381c660da76ef7b8f380c679cace74019898
asharifi90 <asharifibb@gmail.com>
2024-01-11 04:19:11 +0330

    add getter and setter to Book class

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -3,4 +3,27 @@ public class Book {
     private int year;
     private String author;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
