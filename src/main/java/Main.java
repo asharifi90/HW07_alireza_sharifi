c5aecd3a3d1693ecef0689028df332cece46cc52
asharifi90 <asharifibb@gmail.com>
2024-01-11 06:14:15 +0330

    test addBook and register methode for bookservice and authorservice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException, SQLException {
+        BookService bookService = new BookService();
+        bookService.addBook("How to", 98, 1);
+
+        AuthorService authorService = new AuthorService();
+        authorService.register("ali","sh",33);
+    }
 }
