5f2e9df2c136720fe909d4f81ded4b10ce4051aa
asharifi90 <asharifibb@gmail.com>
2024-01-11 06:04:02 +0330

    Complete save methode to insert title, year and author_id

--- src/main/java/BookRepository.java
+++ src/main/java/BookRepository.java
@@ -1,10 +1,30 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class BookRepository {
-    public void save(Book book){
+    private final JDBCConnection jdbcConnection = new JDBCConnection();
+
+    public BookRepository() throws SQLException {
+    }
+
+    public void save(Book book) throws SQLException {
+        Connection connection = jdbcConnection.getConnection();
+
+        String addBook = "INSERT INTO book(title, year, author_id) VALUES(?, ?, ?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(addBook);
+
+
+        preparedStatement.setString(1, book.getTitle());
+        preparedStatement.setInt(2, book.getYear());
+        preparedStatement.setInt(3, book.getAuthorId());
+
+        preparedStatement.executeUpdate();
 
     }
-    public Book load (int bookId){
-
-    }
+//    public Book load (int bookId){
+//
+//    }
     public void delete(Book book){
 
     }
